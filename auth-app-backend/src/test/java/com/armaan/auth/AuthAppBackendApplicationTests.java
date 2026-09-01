c8e31e48543986603a349fdb375216cb1c489be9
armaanmulani <armaanmulani941@gmail.com>
2026-09-02 01:24:41 +0530

    chore: initialize Spring Boot project

---
+++ auth-app-backend/src/test/java/com/armaan/auth/AuthAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armaan.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
